2266a3e945abdf02edbdb943e2c67e00a6c9435d
Kibreab <Akkeba37@gmail.com>
2024-08-21 16:09:18 +0300

    Implement the user routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 connectDB();
+
+app.use("/api", userRoutes)
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log("Server listening on port " + port));
